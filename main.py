74b51a3b83528171e1563e4028bf6061b647f06e
alexey.polyanichko <alexey.polyanichko@gmail.com>
2022-12-28 13:09:28 +0200

    menu logic is moved from main to individual method

--- main.py
+++ main.py
@@ -1,47 +1,4 @@
 import menu
-import data_manager as dm
-
 
 if __name__ == '__main__':
-    invite_text = 'Select a menu number from any of the above: '
-    show_format_menu = True
-    while show_format_menu:
-        menu.print_format_menu()
-
-        try:
-            command = int(input(invite_text))
-
-            if command == 1 or command == 2:
-                dm.init_phonebook(command)  # set format and read contacts from a file
-
-                show_menu = True
-                while show_menu:
-                    menu.print_menu()
-
-                    try:
-                        command = int(input(invite_text))
-
-                        if command == 1:  # [1] Show all contacts
-                            dm.print_contacts()
-                        elif command == 2:  # [2] Search contacts (not implemented)
-                            dm.search_contacts()
-                        elif command == 3:  # [3] Add contact
-                            dm.add_contact()
-                        elif command == 4:  # [4] Delete contact
-                            dm.delete_contact()
-                        elif command == 5:  # [5] Edit contact (not implemented)
-                            dm.edit_contact()
-                        elif command == 0:  # [0] Exit -> format selection
-                            dm.save_contacts()
-                            show_menu = False
-                        else:
-                            menu.print_error()
-                    except ValueError:
-                        menu.print_error()
-
-            elif command == 0:  # [0] Exit -> program
-                show_format_menu = False
-            else:
-                menu.print_error()
-        except ValueError:
-            menu.print_error()
+    menu.start_program()
--- menu.py
+++ menu.py
@@ -1,3 +1,6 @@
+import data_manager as dm
+
+
 def print_format_menu():
     print('Select display format for the "Phone Book":')
     print('[1] In one line')
@@ -18,3 +21,48 @@ def print_menu():
 
 def print_error():
     print('--> Invalid input!')
+
+
+def start_program():
+    invite_text = 'Select a menu number from any of the above: '
+    show_format_menu = True
+    while show_format_menu:
+        print_format_menu()
+
+        try:
+            command = int(input(invite_text))
+
+            if command == 1 or command == 2:
+                dm.init_phonebook(command)  # set format and read contacts from a file
+
+                show_menu = True
+                while show_menu:
+                    print_menu()
+
+                    try:
+                        command = int(input(invite_text))
+
+                        if command == 1:  # [1] Show all contacts
+                            dm.print_contacts()
+                        elif command == 2:  # [2] Search contacts (not implemented)
+                            dm.search_contacts()
+                        elif command == 3:  # [3] Add contact
+                            dm.add_contact()
+                        elif command == 4:  # [4] Delete contact
+                            dm.delete_contact()
+                        elif command == 5:  # [5] Edit contact (not implemented)
+                            dm.edit_contact()
+                        elif command == 0:  # [0] Exit -> format selection
+                            dm.save_contacts()
+                            show_menu = False
+                        else:
+                            print_error()
+                    except ValueError:
+                        print_error()
+
+            elif command == 0:  # [0] Exit -> program
+                show_format_menu = False
+            else:
+                print_error()
+        except ValueError:
+            print_error()
